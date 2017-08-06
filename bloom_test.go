f8ff2484f3abc6f63d339c3e1232a7bb4840d811
Ricardo Geraldes <rgeraldes24@gmail.com>
2017-08-06 02:18:38 +0100

    tests template

---
+++ bloom_test.go
@@ -0,0 +1,25 @@
+package bloom
+
+import (
+	"testing"
+)
+
+func TestNbits(t *testing.T) {
+
+}
+
+func TestNHashes(t *testing.T) {
+
+}
+
+func TestAdd(t *testing.T) {
+
+}
+
+func TestHas(t *testing.T) {
+
+}
+
+func TestClear(t *testing.T) {
+
+}
